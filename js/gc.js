d87d9b690807deb5794151cdf14fb0a15422e394
Daniel Szlaski <szlaski.daniel@gmail.com>
2020-02-12 10:59:14 +0100

    Authenticate against GenesysCloud

---
+++ js/gc.js
@@ -0,0 +1,74 @@
+// **** Token Implicit Grant (Browser) - UserLogin ****
+//let redirectUri = 'https://szlaskidaniel.github.io/purecloud-place-call/index.html';
+redirectUri = 'https://localhost:444/index.html';
+const platformClient = require('platformClient');
+const client = platformClient.ApiClient.instance;
+
+client.setEnvironment("mypurecloud.ie");
+client.setPersistSettings(true);
+
+let userInfo = {
+    name: undefined,
+    mail: undefined,
+    phone: undefined,
+    groups: []
+};
+
+getMe();
+
+
+function getMe() {
+
+    try {
+        // Authenticate
+        client.loginImplicitGrant("42f3a603-79ca-4212-a4b5-7847846ce47e", redirectUri)
+            .then(() => {
+                console.log('Logged-In');
+                let apiInstance = new platformClient.UsersApi();
+                let opts = {
+                    'expand': ["groups"]
+                };
+
+                apiInstance.getUsersMe(opts)
+                    .then((data) => {
+                        console.log(`getUsersMe success! data: ${JSON.stringify(data, null, 2)}`);
+
+                        //Read UserData
+                        userInfo.name = data.name;
+                        userInfo.mail = data.username;
+
+                        data.primaryContactInfo.forEach(function (aItem) {
+                            switch (aItem.mediaType.toLowerCase()) {
+                                case 'email':
+                                    userInfo.mail = aItem.address;
+                                    break;
+                                case 'phone':
+                                    userInfo.phone = aItem.address;
+                                    break;
+                                default:
+                                    break;
+                            }
+                        })
+
+                        data.groups.forEach(function (aItem) {
+                            userInfo.groups.push(aItem.id)
+                        })
+                        console.log(userInfo);
+                        return userInfo
+
+
+                    })
+                    .catch((err) => {
+                        console.log('There was a failure calling getUsersMe');
+                        console.error(err);
+                        return userInfo
+                    });
+
+            })
+    } catch (error) {
+        console.log(error);
+        return userInfo
+    }
+
+}
+
