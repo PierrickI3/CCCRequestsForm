b320f309fac57f4a439c0b0625d95d1fa71be0c4
Daniel Szlaski <szlaski.daniel@gmail.com>
2020-02-12 11:19:03 +0100

    include promises in gc.js and sample call function

--- js/gc.js
+++ js/gc.js
@@ -1,6 +1,6 @@
 // **** Token Implicit Grant (Browser) - UserLogin ****
 //let redirectUri = 'https://szlaskidaniel.github.io/purecloud-place-call/index.html';
-redirectUri = 'https://localhost:444/index.html';
+redirectUri = 'http://localhost:444/index.html';
 const platformClient = require('platformClient');
 const client = platformClient.ApiClient.instance;
 
@@ -14,61 +14,60 @@ let userInfo = {
     groups: []
 };
 
-getMe();
-
-
 function getMe() {
+    console.log('Try to login');
+    return new Promise(async (resolve, reject) => {
+        try {
+            // Authenticate
+            
+            client.loginImplicitGrant("42f3a603-79ca-4212-a4b5-7847846ce47e", redirectUri)
+                .then(() => {
+                    console.log('Logged-In');
+                    let apiInstance = new platformClient.UsersApi();
+                    let opts = {
+                        'expand': ["groups"]
+                    };
 
-    try {
-        // Authenticate
-        client.loginImplicitGrant("42f3a603-79ca-4212-a4b5-7847846ce47e", redirectUri)
-            .then(() => {
-                console.log('Logged-In');
-                let apiInstance = new platformClient.UsersApi();
-                let opts = {
-                    'expand': ["groups"]
-                };
+                    apiInstance.getUsersMe(opts)
+                        .then((data) => {
+                            console.log(`getUsersMe success! data: ${JSON.stringify(data, null, 2)}`);
 
-                apiInstance.getUsersMe(opts)
-                    .then((data) => {
-                        console.log(`getUsersMe success! data: ${JSON.stringify(data, null, 2)}`);
+                            //Read UserData
+                            userInfo.name = data.name;
+                            userInfo.mail = data.username;
 
-                        //Read UserData
-                        userInfo.name = data.name;
-                        userInfo.mail = data.username;
+                            data.primaryContactInfo.forEach(function (aItem) {
+                                switch (aItem.mediaType.toLowerCase()) {
+                                    case 'email':
+                                        userInfo.mail = aItem.address;
+                                        break;
+                                    case 'phone':
+                                        userInfo.phone = aItem.address;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            })
 
-                        data.primaryContactInfo.forEach(function (aItem) {
-                            switch (aItem.mediaType.toLowerCase()) {
-                                case 'email':
-                                    userInfo.mail = aItem.address;
-                                    break;
-                                case 'phone':
-                                    userInfo.phone = aItem.address;
-                                    break;
-                                default:
-                                    break;
-                            }
+                            data.groups.forEach(function (aItem) {
+                                userInfo.groups.push(aItem.id)
+                            })
+                            
+                            resolve(userInfo);
+
+
                         })
+                        .catch((err) => {
+                            console.log('There was a failure calling getUsersMe');
+                            console.error(err);
+                            reject(err);
+                        });
 
-                        data.groups.forEach(function (aItem) {
-                            userInfo.groups.push(aItem.id)
-                        })
-                        console.log(userInfo);
-                        return userInfo
-
-
-                    })
-                    .catch((err) => {
-                        console.log('There was a failure calling getUsersMe');
-                        console.error(err);
-                        return userInfo
-                    });
-
-            })
-    } catch (error) {
-        console.log(error);
-        return userInfo
-    }
-
+                })
+        } catch (error) {
+            console.log(error);
+            reject(error);
+        }
+    });
 }
 
