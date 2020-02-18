f3608f497ad0db661755f98d18428546b5002d3a
Daniel Szlaski <szlaski.daniel@gmail.com>
2020-02-18 13:30:03 +0100

    move disctionaries to file, dynamic product,sf but

---
+++ TODO.md
@@ -0,0 +1,4 @@
+
+What happens on the review page when a request is closed?  We want the item to disappear but like the isDeleted flag, it needs to remain in the DB for reporting purposes.
+I’ve added a field to the requirements document to capture Date/Time Request Raised.  It would be nice to see this on the review requests form.
+Can you make the Requested Date/Time field visible on the review form as well please.
---
+++ js/dictionary.js
@@ -0,0 +1,62 @@
+
+let adminGroups = {
+    "EMEA": [
+        "4bfbc98d-35d6-4a85-85f9-4e661c1a32c0"
+    ],
+    "NA": [
+        "1234"
+    ],
+    "LATAM": [
+        "4bfbc98d-35d6-4a85-85f9-4e661c1a32c0"
+    ],
+    "APAC": [
+        "1234"
+    ]
+};
+
+let regions = {
+    APAC: [
+        "Asia", "Australia", "New Zeland"
+    ],
+    EMEA: [
+        "Central", "North", "South"
+    ],
+    LATAM: [
+        "Andean", "Brazil", "Mexico", "Southern-Cone"
+    ],
+    NA: [
+        "East Enterptise", "East Commercial", "East MM", "Central Enterprise", "Central Commercial", "Central MM", "West Enterprise", "West Commercial", "West MM", "Canada", "Government", "Velocity", "Channels"
+    ]
+}
+
+let requestsType = {
+    // "Genesys Cloud": ["Cloud Migrations", "Innovation (Custom Demos, Integrations, Beta Testing)", "Must Win/Strategic Opportunity Support", "POCs & Pilots for Strategic Opportunities", "Technical Design/Architecture Review", "Other"],
+    EMEA: {
+        "Genesys Cloud": ["Additional Privacy Support", "Additional Security Support", "Associate SC & AE Enablement", "Cloud Migrations", "Critical Situation Management", "End to End Partner Enablement", "Innovation (Custom Demos, Integrations, Beta Testing)", "Must Win/Strategic Opportunity Support", "Partner Enablement (Summits/ Events/ Webinars)", "POC & Pilots for Strategic Opportunities", "Technical Design/ Architecture Review", "Test Drive", "Other"],
+        "Genesys Engage": ["Cloud Migrations", "Innovation (Custom Demos, Integrations, Beta Testing)", "Must Win/Strategic Opportunity Support", "POC & Pilots for Strategic Opportunities", "Technical Design/ Architecture Review", "Other"],
+        "Genesys Engage Cloud": ["Cloud Migrations", "Innovation (Custom Demos, Integrations, Beta Testing)", "Must Win/Strategic Opportunity Support", "POC & Pilots for Strategic Opportunities", "Technical Design/ Architecture Review", "Other"],
+        "PureConnect": ["Additional Privacy Support", "Additional Security Support", "Cloud Migrations", "Technical Design/ Architecture Review", "Other"]
+
+    },
+    APAC: {
+        "Genesys Cloud": ["Cloud Migrations", "Innovation (Custom Demos, Integrations, Beta Testing)", "Must Win/Strategic Opportunity Support", "POCs & Pilots for Strategic Opportunities", "Technical Design/Architecture Review", "Other"],
+        "Genesys Engage": ["Cloud Migrations", "Innovation (Custom Demos, Integrations, Beta Testing)", "Must Win/Strategic Opportunity Support", "POCs & Pilots for Strategic Opportunities", "Technical Design/Architecture Review", "Other"],
+        "Genesys Engage Cloud": ["Cloud Migrations", "Innovation (Custom Demos, Integrations, Beta Testing)", "Must Win/Strategic Opportunity Support", "POCs & Pilots for Strategic Opportunities", "Technical Design/Architecture Review", "Other"],
+        "PureConnect": ["Cloud Migrations", "Innovation (Custom Demos, Integrations, Beta Testing)", "Must Win/Strategic Opportunity Support", "POCs & Pilots for Strategic Opportunities", "Technical Design/Architecture Review", "Other"]
+
+    },
+    LATAM: {
+        "Genesys Cloud": ["Cloud Migrations", "Innovation (Custom Demos, Integrations, Beta Testing)", "Must Win/Strategic Opportunity Support", "POCs & Pilots for Strategic Opportunities", "Technical Design/Architecture Review", "Other"],
+        "Genesys Engage": ["Cloud Migrations", "Innovation (Custom Demos, Integrations, Beta Testing)", "Must Win/Strategic Opportunity Support", "POCs & Pilots for Strategic Opportunities", "Technical Design/Architecture Review", "Other"],
+        "Genesys Engage Cloud": ["Cloud Migrations", "Innovation (Custom Demos, Integrations, Beta Testing)", "Must Win/Strategic Opportunity Support", "POCs & Pilots for Strategic Opportunities", "Technical Design/Architecture Review", "Other"],
+        "PureConnect": ["Cloud Migrations", "Innovation (Custom Demos, Integrations, Beta Testing)", "Must Win/Strategic Opportunity Support", "POCs & Pilots for Strategic Opportunities", "Technical Design/Architecture Review", "Other"]
+
+    },
+    NA: {
+        "Genesys Cloud": ["API Support", "AppFoundry Support", "Business Consulting", "Cloud Migrations", "Data Privacy & Security", "Demo Advanced", "EWM (Work Item Routing)", "POC/Pilot", "Predictive Engagement (Altocloud)", "PureBridge (Don't forget your DSR)", "RFP Advanced", "Service Automation (Self-Service, IVR BOT's)", "Outbound", "WEM/WFM", "Tech Design/Arch Review", "Vision Clip", "Other"],
+        "Genesys Engage": ["API Support", "AppFoundry Support", "Business Consulting", "Cloud Migrations", "Data Privacy & Security", "Demo Advanced", "EWM (Work Item Routing)", "Latitude/Collections", "POC/Pilot", "Predictive Engagement (Altocloud)", "PureBridge (Don't forget your DSR)", "RFP Advanced", "Service Automation (Self-Service, IVR BOT's)", "Outbound", "WEM/WFM", "Tech Design/Arch Review", "Vision Clip", "Other"],
+        "Genesys Engage Cloud": ["API Support", "AppFoundry Support", "Business Consulting", "Cloud Migrations", "Data Privacy & Security", "Demo Advanced", "EWM (Work Item Routing)", "Latitude/Collections", "POC/Pilot", "Predictive Engagement (Altocloud)", "PureBridge (Don't forget your DSR)", "RFP Advanced", "Service Automation (Self-Service, IVR BOT's)", "Outbound", "WEM/WFM", "Tech Design/Arch Review", "Vision Clip", "Other"],
+        "PureConnect": ["API Support", "AppFoundry Support", "Business Consulting", "Cloud Migrations", "Data Privacy & Security", "Demo Advanced", "EWM (Work Item Routing)", "POC/Pilot", "PureBridge (Don't forget your DSR)", "RFP Advanced", "Service Automation (Self-Service, IVR BOT's)", "Outbound", "WEM/WFM", "Tech Design/Arch Review", "Other"]
+
+    }
+}
