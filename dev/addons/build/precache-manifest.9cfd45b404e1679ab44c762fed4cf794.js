6e31cda61dcc230aa17db5b937b3b714d2ffd1fa
Dariusz Socha <dariusz.socha@genesys.com>
2020-08-31 10:35:19 +0200

    push the build folder

---
+++ dev/addons/build/precache-manifest.9cfd45b404e1679ab44c762fed4cf794.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b47dd2c61370501079ec2546b84fc721",
+    "url": "./index.html"
+  },
+  {
+    "revision": "647bebbb8951d9539876",
+    "url": "./static/css/2.25d8efc1.chunk.css"
+  },
+  {
+    "revision": "01c6619a18fe8c93f1b9",
+    "url": "./static/css/main.ef5225fb.chunk.css"
+  },
+  {
+    "revision": "647bebbb8951d9539876",
+    "url": "./static/js/2.699374b8.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "./static/js/2.699374b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c6619a18fe8c93f1b9",
+    "url": "./static/js/main.21ba2dd8.chunk.js"
+  },
+  {
+    "revision": "bf385eb9deb3be971f69",
+    "url": "./static/js/runtime-main.c8f180be.js"
+  }
+]);
