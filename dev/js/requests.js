5faaa610fb3f81853a2f896cc49e2c3fe155be1b
Dariusz Socha <dariusz.socha@genesys.com>
2020-09-01 17:46:19 +0200

    pick requester from the GC search user API

--- dev/js/requests.js
+++ dev/js/requests.js
@@ -459,3 +459,22 @@ async function filterHandleOnMessageEvent(event) {
     refreshGrid();
   }
 }
+
+function getSearchUserBody(pattern) {
+  console.log(`searchUser([${pattern}])`);
+  const patternParsed = pattern.split(' ');
+  let query = [{ type: 'EXACT', fields: ['state'], value: 'active' }];
+  for (const item of patternParsed) {
+    if (!item) continue;
+    query.push({ type: 'QUERY_STRING', fields: ['name', 'email', 'title', 'department'], value: item });
+  }
+  const body = {
+    pageSize: 5,
+    pageNumber: 1,
+    types: ['users'],
+    query,
+    sortOrder: 'ASC',
+    sortBy: 'name',
+  };
+  return body;
+}
