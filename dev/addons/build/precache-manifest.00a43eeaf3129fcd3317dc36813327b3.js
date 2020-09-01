9d360b37b8da135b01f8fc797eaeac1c4c4a3e9e
Daniel Szlaski <szlaski.daniel@gmail.com>
2020-09-01 15:31:01 +0200

    push built version of AdminPage

---
+++ dev/addons/build/precache-manifest.00a43eeaf3129fcd3317dc36813327b3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6251bda459fdece056a629a7501627f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0ab18170d272d14370a5",
+    "url": "./static/css/2.e11bb886.chunk.css"
+  },
+  {
+    "revision": "b8f0c7386d0e92c1c9bf",
+    "url": "./static/css/main.48b7604d.chunk.css"
+  },
+  {
+    "revision": "0ab18170d272d14370a5",
+    "url": "./static/js/2.11f731c9.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "./static/js/2.11f731c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8f0c7386d0e92c1c9bf",
+    "url": "./static/js/main.a8398792.chunk.js"
+  },
+  {
+    "revision": "bf385eb9deb3be971f69",
+    "url": "./static/js/runtime-main.c8f180be.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "./static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "./static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "./static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "./static/media/notification.c0d3c94c.eot"
+  }
+]);
