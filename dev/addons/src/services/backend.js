f10644949f76931bf6d6bd0d27c2f1889ecf552e
Daniel Szlaski <szlaski.daniel@gmail.com>
2020-09-01 15:30:13 +0200

    finish adminPage for emails

--- dev/addons/src/services/backend.js
+++ dev/addons/src/services/backend.js
@@ -1,25 +1,44 @@
 export const searchMailConfiguration = (token, query) => {
-    console.log(`searchMailConfiguration ${query}`);
+  console.log(`searchMailConfiguration ${query}`);
 
-    const body = {
-        "token": token,
-        "region": query.region,
-        "product": query.product
-    };
-    return fetch(`http://localhost:3000/mailconfig`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-    })
-        .then((response) => response.json())
-        .then((response) => {
-            console.log(response);
-            if (response.status < 200 || response.status > 299) throw new Error(JSON.stringify(response));
-            if (response.results && response.results.length > 0) {
-                return response.results;
-            }
-            return [];
-        });
+  const body = {
+    token: token,
+    region: query.region,
+    product: query.product,
+  };
+  return fetch(`http://localhost:3000/mailconfig`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.status < 200 || response.status > 299) throw new Error(JSON.stringify(response));
+      if (response && response.length > 0) {
+        return response;
+      }
+      return [];
+    });
+};
+
+export const updateMailConfiguration = (body) => {
+  console.log(`updateMailConfiguration ${JSON.stringify(body)}`);
+
+  return fetch(`http://localhost:3000/mailconfig`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.status < 200 || response.status > 299) throw new Error(JSON.stringify(response));
+      if (response && response.length > 0) {
+        return response;
+      }
+      return [];
+    });
 };
