260f8b3b9796e1b83bc9584a67ec8e3176271590
Dariusz Socha <dariusz.socha@genesys.com>
2020-09-09 11:58:02 +0200

    rebuild react.js

---
+++ dev/addons/build/precache-manifest.101d29771d51867313470d0d3b635a44.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502163d486c5611477f84c6f986cd24e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "33b5d886a8adce2b47c7",
+    "url": "./static/css/2.028ac7c5.chunk.css"
+  },
+  {
+    "revision": "ced2e161de3bdd5a8f29",
+    "url": "./static/css/main.144e525a.chunk.css"
+  },
+  {
+    "revision": "33b5d886a8adce2b47c7",
+    "url": "./static/js/2.113f36f2.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "./static/js/2.113f36f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ced2e161de3bdd5a8f29",
+    "url": "./static/js/main.360add4f.chunk.js"
+  },
+  {
+    "revision": "bf385eb9deb3be971f69",
+    "url": "./static/js/runtime-main.c8f180be.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "./static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "./static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "./static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "./static/media/notification.c0d3c94c.eot"
+  }
+]);
