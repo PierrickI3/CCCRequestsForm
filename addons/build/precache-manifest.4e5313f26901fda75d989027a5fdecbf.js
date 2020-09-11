4868888399604a79e5f026316a38c7b8aa2cd5b1
PierrickI3 <pierrick.lozach@inin.com>
2020-09-11 09:14:18 +0200

    Apply latest changes to prod

---
+++ addons/build/precache-manifest.4e5313f26901fda75d989027a5fdecbf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c57ddcf5c5a3f8d5d3b1bf9375ec97e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "33b5d886a8adce2b47c7",
+    "url": "./static/css/2.028ac7c5.chunk.css"
+  },
+  {
+    "revision": "89e02b1ac54db4b07cab",
+    "url": "./static/css/main.144e525a.chunk.css"
+  },
+  {
+    "revision": "33b5d886a8adce2b47c7",
+    "url": "./static/js/2.113f36f2.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "./static/js/2.113f36f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89e02b1ac54db4b07cab",
+    "url": "./static/js/main.d0a77494.chunk.js"
+  },
+  {
+    "revision": "bf385eb9deb3be971f69",
+    "url": "./static/js/runtime-main.c8f180be.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "./static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "./static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "./static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "./static/media/notification.c0d3c94c.eot"
+  }
+]);
